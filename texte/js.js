f67778b42984451ddfeec095a4b0033e97e74d96
Jonas-Koni <77193406+Jonas-Koni@users.noreply.github.com>
2021-02-26 19:53:39 +0100

    v-1.4

--- texte/js.js
+++ texte/js.js
@@ -1,1 +1,6 @@
-var height_navigation = getElementById("navigation").
+var navigation = document.getElementById("navigation");
+var information = document.getElementById("information");
+
+var navigation_height = navigation.offsetHeight;
+
+information.style.height = navigation_height - 65;
