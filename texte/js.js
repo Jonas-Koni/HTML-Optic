b7da84c59406935607a9046d07d1555571a8c713
Jonas-Koni <77193406+Jonas-Koni@users.noreply.github.com>
2021-03-08 15:52:37 +0100

    v-1.5

--- texte/js.js
+++ texte/js.js
@@ -1,6 +1,16 @@
 var navigation = document.getElementById("navigation");
 var information = document.getElementById("information");
+var footer = document.getElementById("footer");
 
 var navigation_height = navigation.offsetHeight;
+var footer_height = footer.offsetHeight;
+var footer_style = window.getComputedStyle(footer);
+var footer_marginTop = parseFloat(footer_style.marginTop);
+var information_style = window.getComputedStyle(information);
 
-information.style.height = navigation_height - 65;
+console.log(navigation_height - footer_height - parseFloat(footer_style.marginTop));
+
+information.style.minHeight = navigation_height - footer_height - footer_marginTop - 42; // - parseFloat(information_style.padding) - parseFloat(information_style.padding);
+
+var author_name = document.getElementsByTagName("meta")[0];
+var author_text = document.getElementById("author").innerHTML = "Autor: " + author_name.content;
